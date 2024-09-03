7cf0ad170ddbcba86b7f38d06fb28854257758ac
ellenangelicakasih <101182895+ellenangelicakasih@users.noreply.github.com>
2024-09-04 00:42:57 +0700

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Konfigurasi API dan Token Bot
-API_ID = os.environ.get("API_ID")
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-
-# Direktori untuk menyimpan video yang diunduh
-DOWNLOAD_DIR = "./DOWNLOADS"
